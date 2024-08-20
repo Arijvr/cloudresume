35d7349b9600aa5d77a17a90c7d0ba1b53ba97f7
Arijvr <armandjvr1991@outlook.com>
2024-08-20 18:50:16 +0400

    modified

--- Website/visitor.js
+++ Website/visitor.js
@@ -1,11 +1,11 @@
 
 
     // Javascript code
-const counter = document.querySelector(".visitor-counter");
+const counter = document.querySelector(".counter-number");
 async function updateCounter(){
     let response = await fetch("https://cpvaxvw75jcmfsovpuxxicjyqa0jspvh.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = ' Views: ${data}';
 }
 
 updateCounter();
